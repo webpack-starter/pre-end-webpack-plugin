b0fc3ada1adcfeb3c324c3e4f4d1fd78ef12424e
wanghairong-i <3189578410@qq.com>
2018-07-26 15:40:27 +0800

    first add

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # pre-end-webpack-plugin
 Webpack 即将退出时，根据成功、失败情况分别执行不同的回调操作
+
+## How to use
+
+在webpack配置文件中
+```javascript
+import PreEndWebpackPlugin from 'pre-end-webpack-plugin';
+
+module.exports = {
+  // ... 省略其余代码
+  plugins: [
+    // ... 省略其余代码
+    new PreEndWebpackPlugin(() => {
+      // Webpack 构建成功后的回调
+    }, (err) => {
+      // Webpack 构建失败后的回调，err 是导致错误的原因
+    })
+  ]
+}
+```
---
+++ index.es6.js
@@ -0,0 +1,33 @@
+/**
+ * 在 Webpack 即将退出时，根据成功、失败情况分别执行不同的回调操作
+ * --------------------
+ * how to use ?
+ * 在webpack配置文件中
+ * import PreEndWebpackPlugin from 'pre-end-webpack-plugin';
+ * plugins: [
+ * 	new PreEndWebpackPlugin(() => {
+ * 		// Webpack 构建成功后的回调
+ * 	}, (err) => {
+ * 		// Webpack 构建失败后的回调，err 是导致错误的原因
+ * 	})
+ * ]
+ */
+class PreEndWebpackPlugin {
+	constructor(doneCallback, failCallback){
+		// 实例化时传入两个回调函数
+		this.doneCallback = doneCallback;
+		this.failCallback = failCallback;
+	}
+	apply(compiler) {
+		// done事件，在成功构建并且输出了文件后，Webpack 即将退出时发生；
+		compiler.plugin('done', (stats) => {
+			this.doneCallback && this.doneCallback(stats);
+		});
+		// failed事件，在构建出现异常导致构建失败，Webpack 即将退出时发生；
+		compiler.plugin('failed', (err) => {
+			this.failCallback && this.failCallback(err);
+		});
+	}
+}
+
+module.exports = PreEndWebpackPlugin;
